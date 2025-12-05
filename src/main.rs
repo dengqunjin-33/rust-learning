1bccb3a69ae2aa780b76a5b8d913e3228002b0bb
邓群金 <dengqunjin@vv.cn>
2025-12-05 17:49:39 +0800

    xx

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main(){
+    // let a = add(10, 5);
+    // println!("a value is {}",a);
+
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+    // println!("{}, world!", s2);
+    // //这里报错  value borrowed here after move
+    // println!("{}, world!", s1);
+
+    
+}
+
+
+
+// fn add(i:i32,j:i32) -> i32{
+//     i+j
+// }
